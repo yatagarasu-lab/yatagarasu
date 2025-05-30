98eb4cb32f03c1ffc07639f66ac21239a8610593
sro-t <egasira.0909@icloud.com>
2025-05-30 23:11:08 +0900

    main.py を更新

--- main.py
+++ main.py
@@ -1,37 +1,39 @@
-import os
 from flask import Flask, request, abort
-from linebot import LineBotApi, WebhookHandler
-from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from linebot.v3.messaging import MessagingApiClient, ReplyMessageRequest, TextMessage
+from linebot.v3.webhook import WebhookHandler
+from linebot.v3.webhooks import MessageEvent
+import os
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi(os.environ["LINE_CHANNEL_ACCESS_TOKEN"])
+# 環境変数からトークンとシークレットを取得
+line_bot_api = MessagingApiClient(channel_access_token=os.environ["LINE_CHANNEL_ACCESS_TOKEN"])
 handler = WebhookHandler(os.environ["LINE_CHANNEL_SECRET"])
 
-@app.route("/")
-def hello():
-    return "LINE Bot is running!"
-
-@app.route("/callback", methods=["POST"])
+@app.route("/callback", methods=['POST'])
 def callback():
-    signature = request.headers["X-Line-Signature"]
+    signature = request.headers.get('X-Line-Signature')
     body = request.get_data(as_text=True)
 
     try:
         handler.handle(body, signature)
-    except InvalidSignatureError:
+    except Exception as e:
         abort(400)
 
-    return "OK"
+    return 'OK'
 
-@handler.add(MessageEvent, message=TextMessage)
+# メッセージイベントを処理
+@handler.add(MessageEvent)
 def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text)
-    )
+    if isinstance(event.message, TextMessage):
+        message = TextMessage(text="こんにちは！")
+        line_bot_api.reply_message(
+            ReplyMessageRequest(
+                reply_token=event.reply_token,
+                messages=[message]
+            )
+        )
 
+# Flaskアプリを gunicorn が認識できるように定義
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=10000)
