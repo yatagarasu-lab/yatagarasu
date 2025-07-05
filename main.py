2af0e3716d1684e1152837f26a9358216661fa97
sro-t <egasira.0909@icloud.com>
2025-07-05 09:16:05 +0900

    main.py を更新

--- main.py
+++ main.py
@@ -1,29 +1,22 @@
 from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, ImageMessage
+from linebot.models import MessageEvent, TextMessage
 
-from handlers.handle_text_message import handle_text_message
-from handlers.handle_image_message import handle_image_message
+from handle_text_message import handle_text_message
 
 import os
 
-# 環境変数からアクセストークンとシークレットを取得
-LINE_CHANNEL_ACCESS_TOKEN = os.environ.get("LINE_CHANNEL_ACCESS_TOKEN")
-LINE_CHANNEL_SECRET = os.environ.get("LINE_CHANNEL_SECRET")
-
 app = Flask(__name__)
 
-line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
-handler = WebhookHandler(LINE_CHANNEL_SECRET)
+# 環境変数からLINEチャネル情報を取得
+line_bot_api = LineBotApi(os.environ.get("LINE_CHANNEL_ACCESS_TOKEN"))
+handler = WebhookHandler(os.environ.get("LINE_CHANNEL_SECRET"))
 
 
-@app.route("/callback", methods=["POST"])
+@app.route("/callback", methods=['POST'])
 def callback():
-    # X-Line-Signatureヘッダーから署名を取得
-    signature = request.headers["X-Line-Signature"]
-
-    # リクエストボディを取得
+    signature = request.headers['X-Line-Signature']
     body = request.get_data(as_text=True)
 
     try:
@@ -31,15 +24,12 @@ def callback():
     except InvalidSignatureError:
         abort(400)
 
-    return "OK"
+    return 'OK'
 
 
-@handler.add(MessageEvent)
+@handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    if isinstance(event.message, TextMessage):
-        handle_text_message(event, line_bot_api)
-    elif isinstance(event.message, ImageMessage):
-        handle_image_message(event, line_bot_api)
+    handle_text_message(event, line_bot_api)
 
 
 if __name__ == "__main__":
