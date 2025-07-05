e4b73c263bcef0b11651545afca86bdc2b98592c
sro-t <egasira.0909@icloud.com>
2025-07-05 09:22:30 +0900

    main.py を更新

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
+# main.py
+
+import os
 from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage
-
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage, ImageMessage
+)
 from handle_text_message import handle_text_message
-
-import os
+from handle_image_message import handle_image_message
 
 app = Flask(__name__)
 
-# 環境変数からLINEチャネル情報を取得
-line_bot_api = LineBotApi(os.environ.get("LINE_CHANNEL_ACCESS_TOKEN"))
-handler = WebhookHandler(os.environ.get("LINE_CHANNEL_SECRET"))
+# LINE APIキーとシークレット
+LINE_CHANNEL_ACCESS_TOKEN = os.environ.get("LINE_CHANNEL_ACCESS_TOKEN")
+LINE_CHANNEL_SECRET = os.environ.get("LINE_CHANNEL_SECRET")
 
+line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(LINE_CHANNEL_SECRET)
 
-@app.route("/callback", methods=['POST'])
+@app.route("/callback", methods=["POST"])
 def callback():
-    signature = request.headers['X-Line-Signature']
+    signature = request.headers["X-Line-Signature"]
     body = request.get_data(as_text=True)
 
     try:
@@ -24,13 +29,17 @@ def callback():
     except InvalidSignatureError:
         abort(400)
 
-    return 'OK'
+    return "OK"
 
-
+# テキストメッセージ受信時の処理
 @handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
+def handle_text(event):
     handle_text_message(event, line_bot_api)
 
+# 画像メッセージ受信時の処理
+@handler.add(MessageEvent, message=ImageMessage)
+def handle_image(event):
+    handle_image_message(event, line_bot_api)
 
 if __name__ == "__main__":
     app.run()
